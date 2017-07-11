dff3ada88b6bc50ac784ec6d926bfe5311d99c60
Hironsan <light.tree.1.13@gmail.com>
2017-07-11 16:11:29 +0900

    implement function for data api

--- arxivtimes_indicator/models/model.py
+++ arxivtimes_indicator/models/model.py
@@ -46,3 +46,60 @@ class Label(BaseModel):
     def __init__(self, name):  # Aggregate label names
         super(Label, self).__init__()
         self.name = name
+
+
+def get_recent(user_id, limit):
+    Issue.select().where(Issue.user_id==user_id).order_by(Issue.created_at)
+
+
+def get_popular(user_id, limit):
+    Issue.select().where(Issue.user_id==user_id).order_by(Issue.score)
+
+
+def aggregate_per_month(self, user_id="", month=6, use_genre=True):
+    """
+    Get aggreagation of post count per year_month and genre or label
+    Args:
+        user_id: to filter the records by user_id
+        month: to limit the aggrecation time range
+        use_genre: use genre to aggregate (when False then use label)
+    Returns:
+        aggregation: post count aggregation by year/month, genre(or label).
+        example {"2017/01": {"genre1": 1, "genre2": 3, ...}}
+        (The size of each year/month aggregation should be equal.
+        You have to compensate 0 if the specific genres don't exist in that year/month.)
+    """
+
+    from datetime import datetime
+    import dateutil.relativedelta
+    import dateutil.parser
+    from collections import defaultdict, Counter
+    now = datetime.now()
+    start_time = now - dateutil.relativedelta(months=month)
+    start_time_str = start_time.strftime("%Y-%m-%d %H:%M:%S+00:00")
+    issues = Issue.select().where(Issue.user_id == user_id).where(Issue.created_at >= start_time_str)
+    counter = defaultdict(Counter)
+    for issue in issues:
+        key = dateutil.parser.parse(issue.created_at).strftime('%Y/%m')
+        for label in issue.labels:
+            counter[key][label] += 1
+    return counter
+
+
+def aggregate_kinds(user_id="", month=6, use_genre=True):
+    """
+    Get aggreagation of post count per genre or label
+    Args:
+        user_id: to filter the records by user_id
+        month: to limit the aggrecation time range
+        use_genre: use genre to aggregate (when False then use label)
+    Returns:
+        aggregation: post count aggregation by genre(or label).
+        example {"genre1": 11, "genre2": 31, ...}
+    """
+    from collections import Counter
+    counters = aggregate_per_month(user_id, month, use_genre)
+    counter = Counter()
+    for c in counters.values():
+        counter += c
+    return counter
